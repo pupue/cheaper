13c1ecf1a9ffb27f25b11e69eae18bf58ca7ef1e
komiyama029 <komiyama@ekanlab.co.jp>
2024-06-28 18:05:03 +0900

    icon追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,13 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Cheaper than",
   description: "どっちの方が安いか比較します",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/icon-192x192.png", type: "image/png", sizes: "192x192" },
+    ],
+    apple: [{ url: "/apple-icon-180x180.png", type: "image/png", sizes: "180x180" }],
+  },
 };
 
 export default function RootLayout({
