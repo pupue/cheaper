7f7c1ce564c6afb0fdcb9d07559f8d1be01ba9e0
pupue <reika02955@gmail.com>
2025-08-21 15:31:04 +0900

    タイトル変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,27 +5,27 @@ import "./globals.css";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Cheaper than",
-  description: "どっちの方が安いか比較します",
-  icons: {
-    icon: [
-      { url: "/favicon.ico", type: "image/x-icon" },
-      { url: "/icon-192x192.png", type: "image/png", sizes: "192x192" },
-    ],
-    apple: [{ url: "/apple-icon-180x180.png", type: "image/png", sizes: "180x180" }],
-  },
+	title: "cheaper",
+	description: "どっちの方が安いか比較します",
+	icons: {
+		icon: [
+			{ url: "/favicon.ico", type: "image/x-icon" },
+			{ url: "/icon-192x192.png", type: "image/png", sizes: "192x192" },
+		],
+		apple: [
+			{ url: "/apple-icon-180x180.png", type: "image/png", sizes: "180x180" },
+		],
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body className={montserrat.className}>
-			{children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<body className={montserrat.className}>{children}</body>
+		</html>
+	);
 }
